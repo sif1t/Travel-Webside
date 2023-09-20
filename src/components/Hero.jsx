acc51b4b4bf15f5bbcd0effea7961ff1643bd90a
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-20 19:54:34 +0600

    searce  section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,13 @@ const Hero = () => {
   return (
     <div className='w-full h-screen relative'>
           <video className='w-full h-full object-cover' src={beachvid} 
-          autoPlay
-           loop 
-           muted/>
+          autoPlay loop  muted />
+          <div className=' absolute w-full h-full top-0 left-0 bg-gray-900/30'>
+            <div className=' absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+                <h1>Nice Travel</h1>
+                <h2>Top 1% Locations Worldwide</h2>
+            </div>
+          </div>
     </div>
   )
 }
