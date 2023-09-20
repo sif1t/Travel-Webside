b3b7ebab65d36a06ee50af2f1f3ad12a785ad704
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-20 18:52:13 +0600

    menu open and close

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import {BiSearch} from 'react-icons/bi'
-import {VscAccount} from 'react-icons/vsc'
-import {HiOutlineMenuAlt4} from 'react-icons/hi'
-import {FaFacebook, FaTwitter, FaPinterest, FaInstagram, FaYoutube} from'react-icons/fa'
+import React, {useState} from 'react';
+import {BiSearch} from 'react-icons/bi';
+import {VscAccount} from 'react-icons/vsc';
+import {HiOutlineMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+import {FaFacebook, FaTwitter, FaPinterest, FaInstagram, FaYoutube} from'react-icons/fa';
 
 const Navbar = () => {
-  return (
+    const [nav, setNav] = useState (false)
+    const handleNav = () => {
+        setNav (!nav)
+    }
+   return (
     <div className='flex justify-between items-center h-20 px-4'>
        <div>
           <h1>BEACHES.</h1>
@@ -23,24 +28,25 @@ const Navbar = () => {
        </div>
 
      {/* hamburger  */}
-       <div className=' md:hidden'>
-         <HiOutlineMenuAlt4 size={20}/>
+       <div onClick={handleNav} className=' md:hidden'>
+         {nav ? <AiOutlineClose size={20} /> : <HiOutlineMenuAlt4 size={20}/>
+}
        </div>
 
        {/* mobile menu dropdown  */}
-       <div className=' absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col'>
+       <div className=' absolute left-[-100%] top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col'>
          <ul>
             <h1>BEACHSE.</h1>
-         <li>Home</li>
-         <li>Destinations</li>
-         <li>Travel</li>
-         <li>View</li>
-         <li>Book</li>
-         <div>
-             <button>Search</button>
+         <li className=' border-b'>Home</li>
+         <li className=' border-b'>Destinations</li>
+         <li className=' border-b'>Travel</li>
+         <li className=' border-b'>View</li>
+         <li className=' border-b'>Book</li>
+         <div className='flex flex-col'>
+             <button className='my-6'>Search</button>
              <button>Account</button>
          </div>
-         <div>
+         <div className='flex justify-between my-6'>
             <FaFacebook className='icon' />
             <FaTwitter  className='icon'/>
             <FaYoutube className='icon' />
